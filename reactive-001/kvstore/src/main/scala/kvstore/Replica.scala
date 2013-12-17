f82b400fee541fe7a032609264520372b598032a
Fox <fox@thefox.eu>
2013-12-17 19:41:49 +0000

    adding kvstore for reactive

---
+++ reactive-001/kvstore/src/main/scala/kvstore/Replica.scala
@@ -0,0 +1,101 @@
+package kvstore
+
+import akka.actor.{ OneForOneStrategy, Props, ActorRef, Actor }
+import kvstore.Arbiter._
+import scala.collection.immutable.Queue
+import akka.actor.SupervisorStrategy.Restart
+import scala.annotation.tailrec
+import akka.pattern.{ ask, pipe }
+import akka.actor.Terminated
+import scala.concurrent.duration._
+import akka.actor.PoisonPill
+import akka.actor.OneForOneStrategy
+import akka.actor.SupervisorStrategy
+import akka.util.Timeout
+import akka.actor.ActorLogging
+
+object Replica {
+  sealed trait Operation {
+    def key: String
+    def id: Long
+  }
+  case class Insert(key: String, value: String, id: Long) extends Operation
+  case class Remove(key: String, id: Long) extends Operation
+  case class Get(key: String, id: Long) extends Operation
+
+  sealed trait OperationReply
+  case class OperationAck(id: Long) extends OperationReply
+  case class OperationFailed(id: Long) extends OperationReply
+  case class GetResult(key: String, valueOption: Option[String], id: Long) extends OperationReply
+
+  def props(arbiter: ActorRef, persistenceProps: Props): Props = Props(new Replica(arbiter, persistenceProps))
+}
+
+class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor with ActorLogging {
+  import Replica._
+  import Replicator._
+  import Persistence._
+  import context.dispatcher
+
+  /*
+   * The contents of this actor is just a suggestion, you can implement it in any way you like.
+   */
+  
+  var kv = Map.empty[String, String]
+  var expectedSeq = 0
+  
+  // a map from secondary replicas to replicators
+  var secondaries = Map.empty[ActorRef, ActorRef]
+  // the current set of replicators
+  var replicators = Set.empty[ActorRef]
+
+  override def preStart() {
+    arbiter ! Join
+  }
+  
+  def receive = {
+    case JoinedPrimary   => context.become(leader)
+    case JoinedSecondary => context.become(replica)
+  }
+
+  /* TODO Behavior for  the leader role. */
+  val leader: Receive = {
+    case Get(key, id) => getValue(sender, key, id)
+    case Insert(key, value, id) => {
+      kv += (key -> value)
+      sender ! OperationAck(id)
+    }
+    case Remove(key, id) => {
+      kv -= (key)
+      sender ! OperationAck(id)
+    }
+  }
+
+  /* TODO Behavior for the replica role. */
+  val replica: Receive = {
+    case Get(key, id) => getValue(sender, key, id)
+    case Snapshot(key, valueOption, seq) => {
+      seq.compare(expectedSeq) match {
+        case cmp if cmp > 0 => log.warning(s"ignoring future sequence number [rec/exc]: [$seq/$expectedSeq]")
+        case cmp if cmp < 0 => {
+          log.warning(s"ignoring outdated sequence number [rec/exc]: [$seq/$expectedSeq]")
+          sender ! SnapshotAck(key, seq)
+        }
+        case _ => {
+          valueOption match {
+            case Some(value) => kv += (key -> value)
+            case None => kv -= key
+          }
+          expectedSeq += 1
+          sender ! SnapshotAck(key, seq)
+        }
+      }
+    }
+  }
+  
+  private[this] def getValue(requester: ActorRef, key: String, id: Long) = {
+    log.info(s"got key $key for id $id: ${kv.get(key)}")
+    requester ! GetResult(key, kv.get(key), id)
+  }
+
+}
