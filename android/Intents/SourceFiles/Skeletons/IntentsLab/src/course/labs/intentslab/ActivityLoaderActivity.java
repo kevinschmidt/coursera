3fe74c2fc646223186c335b8068ecb200603b9d0
Fox <fox@thefox.eu>
2014-02-16 23:32:52 +0000

    * solve intents

--- android/Intents/SourceFiles/Skeletons/IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
+++ android/Intents/SourceFiles/Skeletons/IntentsLab/src/course/labs/intentslab/ActivityLoaderActivity.java
@@ -65,10 +65,8 @@ public class ActivityLoaderActivity extends Activity {
 
 		Log.i(TAG,"Entered startExplicitActivation()");
 		
-		// TODO - Create a new intent to launch the ExplicitlyLoadedActivity class
-		
-		// TODO - Start an Activity using that intent and the request code defined above
-
+		Intent i = new Intent(this, ExplicitlyLoadedActivity.class);
+		startActivityForResult(i, GET_TEXT_REQUEST_CODE);
 
 	}
 
@@ -78,19 +76,10 @@ public class ActivityLoaderActivity extends Activity {
 
 		Log.i(TAG, "Entered startImplicitActivation()");
 
-		// TODO - Create a base intent for viewing a URL 
-		// (HINT:  second parameter uses parse() from the Uri class)
-		
-		
-		// TODO - Create a chooser intent, for choosing which Activity
-		// will carry out the baseIntent. Store the Intent in the 
-		// chooserIntent variable below. HINT: using the Intent class' 
-		// createChooser())
-		
-		Intent chooserIntent = null;
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+		Intent chooserIntent = Intent.createChooser(intent, "pick browser");
 
 		Log.i(TAG,"Chooser Intent Action:" + chooserIntent.getAction());
-		// TODO - Start the chooser Activity, using the chooser intent
 		startActivity(chooserIntent);
 
 	}
@@ -100,10 +89,9 @@ public class ActivityLoaderActivity extends Activity {
 
 		Log.i(TAG, "Entered onActivityResult()");
 		
-		// TODO - Process the result only if this method received both a
-		// RESULT_OK result code and a recognized request code
-		// If so, update the Textview showing the user-entered text.
-
+		if (requestCode == GET_TEXT_REQUEST_CODE && resultCode == RESULT_OK) {
+			mUserTextView.setText(data.getStringExtra(TAG));
+		}
 
 	}
 }
--- android/Intents/SourceFiles/Skeletons/IntentsLab/src/course/labs/intentslab/ExplicitlyLoadedActivity.java
+++ android/Intents/SourceFiles/Skeletons/IntentsLab/src/course/labs/intentslab/ExplicitlyLoadedActivity.java
@@ -45,14 +45,10 @@ public class ExplicitlyLoadedActivity extends Activity {
 	private void enterClicked() {
 
 		Log.i(TAG,"Entered enterClicked()");
-		
-		// TODO - Save user provided input from the EditText field
-
-		// TODO - Create a new intent and save the input from the EditText field as an extra
-		
-		// TODO - Set Activity's result with result code RESULT_OK
-		
-		// TODO - Finish the Activity
-
+		String resultText = mEditText.getText().toString();
+		Intent result = new Intent();
+		result.putExtra(TAG, resultText);
+		setResult(RESULT_OK, result);
+		finish();
 	}
 }
