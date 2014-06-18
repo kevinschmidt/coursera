7f7c599377016ee7ef31add6aa8a6447837cdf5f
Fox <fox@thefox.eu>
2014-06-18 21:30:49 +0100

    first two unit tests

--- progfun/streams/src/main/scala/streams/StringParserTerrain.scala
+++ progfun/streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,9 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = { p =>
+    levelVector.lift(p.x).flatMap(_.lift(p.y)).map(_ => true).getOrElse(false)
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +64,11 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val x = levelVector.indexWhere(_.indexWhere(_ == c) != -1)
+    val y = levelVector(x).indexWhere(_ == c)
+    Pos(x, y)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
