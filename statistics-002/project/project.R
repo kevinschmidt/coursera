e5818978e15ad63eab115fe12a3d5c304ce60c04
Fox <fox@thefox.eu>
2014-09-21 00:52:44 +0100

    stats2 lab2 and project

---
+++ statistics-002/project/project.R
@@ -0,0 +1,10 @@
+load(url("http://bit.ly/dasi_gss_data"))
+names(gss)
+summary(gss$coninc)
+hist(gss$coninc)
+summary(gss$marital)
+barplot(summary(gss$marital))
+plot(gss$marital, gss$coninc)
+
+gss_subset <- subset(gss, select=c(gss$coninc, gss$marital))
+head(gss_subset, n=30)
